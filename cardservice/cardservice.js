82c9a1d374fd4225980b7fddcacb987de386e8aa
ZeroSinisi <zero.sinisi@gmail.com>
2019-04-08 00:02:54 -0700

    Card service streams data so that it doesn't run out of memory.

--- cardservice/cardservice.js
+++ cardservice/cardservice.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const fs = require('fs');
+const StreamArray = require('stream-json/streamers/StreamArray');
 
-const cardData = require("./paddata_processed_na_cards.json");
-const cardDataById = {};
-Object.values(cardData).forEach(element => {
-  cardDataById[element.card.card_id] = element;
+const pipeline = fs.createReadStream('paddata_processed_na_cards.json').pipe(StreamArray.withParser());
+const cardData = {};
+
+pipeline.on('data', data => {
+  cardData[data.value.card.card_id] = data.value;
 });
-
-app.get('/cards/:cardId', (req, res) => {
-  res.send(cardDataById[req.params.cardId]);
+pipeline.on('end', () => {
+  app.get('/', (req, res) => {
+    res.send('API List:\nGET /cards/:cardId');
+  });
+  
+  app.get('/cards/:cardId', (req, res) => {
+    res.send(cardData[req.params.cardId]);
+  });
+  
+  app.listen(port, () => console.log(`Card Service listening on port ${port}!`));
 });
-
-app.listen(port, () => console.log(`Card Service listening on port ${port}!`));
