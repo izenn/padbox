0375c14fbb501806e6078b628bcf6268ab16ff1b
ZeroSinisi <zero.sinisi@gmail.com>
2019-04-08 00:13:39 -0700

    Reading from disk every time because js is still running out of memory.

--- cardservice/cardservice.js
+++ cardservice/cardservice.js
@@ -4,20 +4,32 @@ const port = process.env.PORT || 3000;
 const fs = require('fs');
 const StreamArray = require('stream-json/streamers/StreamArray');
 
-const pipeline = fs.createReadStream('paddata_processed_na_cards.json').pipe(StreamArray.withParser());
-const cardData = {};
+function getCardById(id) {
+  let resolver;
+  const promise = new Promise((res, rej) => {
+    resolver = res;
+  });
+  const pipeline = fs.createReadStream('paddata_processed_na_cards.json').pipe(StreamArray.withParser());
+  let ret = null;
+  pipeline.on('data', data => {
+    if(data.value.card.card_id === id){
+      ret = data.value;
+    }
+  });
+  pipeline.on('end', () => {
+    resolver(ret);
+  })
+  return promise;
+}
 
-pipeline.on('data', data => {
-  cardData[data.value.card.card_id] = data.value;
+app.get('/', (req, res) => {
+  res.send('API List:\nGET /cards/:cardId');
 });
-pipeline.on('end', () => {
-  app.get('/', (req, res) => {
-    res.send('API List:\nGET /cards/:cardId');
-  });
-  
-  app.get('/cards/:cardId', (req, res) => {
-    res.send(cardData[req.params.cardId]);
-  });
-  
-  app.listen(port, () => console.log(`Card Service listening on port ${port}!`));
+
+app.get('/cards/:cardId', (req, res) => {
+  getCardById(Number.parseInt(req.params.cardId)).then(cardData => {
+    res.send(cardData);
+  })
 });
+
+app.listen(port, () => console.log(`Card Service listening on port ${port}!`));
