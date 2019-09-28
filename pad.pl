izenn/padbox

commit 29113d99edde615fb0b1f103a05ff69d4029d43b

potential fix for issue 5

pad.pl
@@ -413,7 +413,7 @@ for (my $c = 0; $c < $cardcount; $c++ ) {
   $evotree = do { local $/; <$fh> };
   close($fh);
   $evotree =~ s/\>/&gt;/g;
-  $evotree =~ s/([0-9]{1,4} )$decodedmonster->[$cardnum]{'card'}{'name'}/<strong>$1$decodedmonster->[$cardnum]{'card'}{'name'}<\/strong>/;
+  $evotree =~ s/([0-9]{1,4} ).*/<strong>$1$decodedmonster->[$cardnum]{'card'}{'name'}<\/strong>/;
   $evotree =~ s/"/'/g;
   $monsterline .= "<td><input type='image' data-toggle='tooltip' data-html='true' data-placement='right' src='/images/misc/evobutton.png' title=\"<pre>" . $evotree . "</pre>\"</td>";
   my $skillmax;

commit 093b9f50381b87b8565deebca4f2d2cab78ef971

fix regression caused by 29113d9

pad.pl
@@ -413,7 +413,9 @@ for (my $c = 0; $c < $cardcount; $c++ ) {
   $evotree = do { local $/; <$fh> };
   close($fh);
   $evotree =~ s/\>/&gt;/g;
-  $evotree =~ s/([0-9]{1,4} ).*/<strong>$1$decodedmonster->[$cardnum]{'card'}{'name'}<\/strong>/;
+  my $number = $cardnum;
+  $number += 0;
+  $evotree =~ s/($number .*)/<strong>$1<\/strong>/;
   $evotree =~ s/"/'/g;
   $monsterline .= "<td><input type='image' data-toggle='tooltip' data-html='true' data-placement='right' src='/images/misc/evobutton.png' title=\"<pre>" . $evotree . "</pre>\"</td>";
   my $skillmax;