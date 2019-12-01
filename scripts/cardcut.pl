356c04010de396ba30b8e9460ab6d9aa606dd0dd
izenn <adam@izenn.net>
2019-11-30 21:38:35 -0600

    updated cardcut.pl script to deal with USA image oddities

--- scripts/cardcut.pl
+++ scripts/cardcut.pl
@@ -16,27 +16,36 @@ foreach my $bc (@bcarray) {
 
 `mv data/USA/bc/CARD*.PNG cards`;
 
+#Fix USA image weirdness
+`convert cards/CARDS_014.PNG -resize 1014x1014 -background transparent -gravity north -extent 1014x1014 cards/output.png`;
+`mv cards/output.png cards/CARDS_014.PNG`;
+`convert cards/CARDS_015.PNG -resize 1014x810 -background transparent -gravity south -extent 1014x810 cards/output.png`;
+`mv cards/output.png cards/CARDS_015.PNG`;
+`convert cards/CARDS_019.PNG -resize 1014x1014 -background transparent -gravity south -extent 1014x1014 cards/output.png`;
+`mv cards/output.png cards/CARDS_019.PNG`;
+`convert cards/CARDS_020.PNG -resize 1014x1014 -background transparent -gravity south -extent 1014x1014 cards/output.png`;
+`mv cards/output.png cards/CARDS_020.PNG`;
+
 my @cardarray = `ls cards/CARDS*.PNG`;
 chomp @cardarray;
 
+my $num=1;
+
 foreach my $filename (@cardarray) {
   my $front = $filename;
-  $front =~ s/cards\/CARDS_[0]+//;
-  $front =~ s/.PNG$//;
-  $front = $front - 1;
 
+  print $filename . "\n";
   for (my $y=0; $y < 10; $y++) {
     my $yoffset = 96*$y+6*$y;
     for (my $x=0; $x < 10; $x++) {
       my $xoffset = 96*$x+6*$x;
-      my $end = $x;
-      my $num = "$front$y$end";
-      $num = $num + 1;
       my $cardnum = sprintf("%05d",$num);
       print $cardnum . "\n";
       my $cmd = "convert $filename -crop 96x96+$xoffset+$yoffset cards/card_$cardnum.png";
       `$cmd`;
+      $num++
     }
   }
   `rm $filename`;
 }
+
